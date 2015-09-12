2b88aa42841f1f94ed4fef414633b0a947cbb5c6
Kong Kao <kong0107@gmail.com>
2015-09-13 04:20:30 +0800

    Update Function.concatenate.js

--- Function.concatenate.js
+++ Function.concatenate.js
@@ -1,16 +1,18 @@
 /**
- * Create a function which run the input funcions sequentially.
+ * Create a function which runs the input funcions sequentially.
+ * Usage: var func = Function.concatenate(func1, func2, ...);
+ * Usage: var func = Function.concatenate([func1, func2, ...]);
  *
- * Note that "This function" does NOT run the "input functions".
- * The "returned function" WILL run the "input functions" of "this function".
+ * Note that "this function" does NOT run the "input functions". It's the
+ * "returned function" that runs the "input functions" of "this function".
  * Arguments for "input functions" are needed only when "returned function"
- * is run.
+ * is executed.
  * Input functions can be assigned by an argument list, or you can put all
  * input functions in an array and treat the array as the only argument of 
  * this function.
- * When calling the "returned function", the first argument would be parsed
- * to the first "input function", the second argument to the second "input
- * function", and so on.
+ * When calling the returned function, the first argument would be parsed
+ * to the first input function, the second argument to the second input
+ * function, and so on.
  *
  * This function shall not be named as `concat`; 
  * otherwise String.prototype.concat would be overrided.
