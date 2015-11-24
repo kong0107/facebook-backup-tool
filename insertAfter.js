57ffc85f84501379693e3a665aafbd0cde07b263
Kong Kao <kong0107@gmail.com>
2015-11-24 17:54:31 +0800

    move js files to `js` directory

--- insertAfter.js
+++
@@ -1,5 +0,0 @@
-if(typeof Node.prototype.insertAfter == 'undefined')
-	Node.prototype.insertAfter = function(newnode, existingnode) {
-		if(existingnode == this.lastChild) this.appendChild(newnode);
-		this.insertBefore(newnode, existingnode.nextSibling);
-	};
