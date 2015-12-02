0c4da582049093a834fb6c6b1a1cdb1b62c1afd6
Kong Kao <kong0107@gmail.com>
2015-12-02 13:32:56 +0800

    a more general structure

---
+++ js/controller.js
@@ -0,0 +1,108 @@
+angular.module("myApp", [])
+.config(function($locationProvider) {
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
+})
+.controller("main", function($scope, $http, $location) {
+	$scope.ret = function() {
+		/// Initialization
+		var notFound = function(r) {
+			//console.log(r.status + " " + r.statusText + " on " + r.config.url);
+		}
+		var tabs = [//"info",
+			"albums", "likes", "events",
+			"feed", "posts", "photos", "videos", "tagged",
+			"members", "docs", "comments"
+		];
+		var ret = {
+			tabs: tabs,
+			tab: "albums",
+			months: {},
+			month: {},
+			search: {},
+			photosInAlbum: {}
+		};
+
+		/**
+		 * Get distinct month of `created_time` in an array.
+		 */
+		var groupByMonth = function(arr, tab) {
+			arr.sort(function(a,b) {
+				return new Date(b.created_time) - new Date(a.created_time);
+			});
+			var ms = [];
+			for(var i = 0; i < arr.length; ++i)
+				ms.push(arr[i].created_time.substr(0, 7));
+			ret.months[tab] = $.unique(ms);
+			ret.month[tab] = ret.months[tab][0];
+			ret.search[tab] = "";
+		}
+
+		/**
+		 * Get info and edges.
+		 */
+		for(var i = 0; i < tabs.length; ++i) ret[tabs[i]] = [];
+		$http.get("data/json/main.json").then(function(r) {
+			var id = r.data.id;
+			var type = r.data.type;
+			ret.info = r.data.info;
+			for(var i = 0; i < tabs.length; ++i) {
+				(function() {
+					var tab = tabs[i];
+					$http.get("data/json/"
+						+ type + "_" + id + "_" + tab
+						+ ".json"
+					).then(function(r) {
+						ret[tab] = r.data;
+						if(tab == "posts" || tab == "feed" || tab == "comment")
+							groupByMonth(ret[tab], tab);
+						console.log("Edge " + tab + " is loaded.");
+					}, notFound);
+				})();
+			}
+		}, notFound);
+
+		/**
+		 * Create a link to Facebook by ID of the node.
+		 */
+		ret.createLink = function(id, type) {
+			if(typeof id == "number") id += "";
+			else if(typeof id != "string") id = "";
+			else if(typeof type != "undefined") 
+				id = id.replace("_", "/" + type + "/");
+			return "https://www.facebook.com/" + id;
+		};
+		
+		/**
+		 * Get photos in a album.
+		 */
+		ret.getPhotosInAlbum = function(id) {
+			if($.isArray(ret.photosInAlbum[id])) return;
+			$http.get("data/json/album_" + id + "_photos.json")
+			.then(function(r) {
+				ret.photosInAlbum[id] = r.data;
+			}, notFound);
+		}
+		
+		/**
+		 * Just for debug
+		 */
+		ret.templates = [
+			"body_header",
+			"post_body",
+			"album_body",
+			"comment_body",
+			"photo_body",
+			"attachment_body"
+		];
+
+		return ret;
+	}();
+})
+.filter("capitalize", function() {
+	return function(input) {
+		return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1) : '';
+	};
+});
