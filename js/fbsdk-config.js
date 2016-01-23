a0a48c80a263ec24b094b5990b265e8150ae11bc
Kong Kao <kong0107@gmail.com>
2016-01-23 14:16:34 +0800

    update FB.appId

--- js/fbsdk-config.js
+++ js/fbsdk-config.js
@@ -1,6 +1,6 @@
 var FBConfig = {
 	version    : 'v2.5',
-	appId      : '875679652530913',
+	appId      : (location.hostname == 'localhost') ? '875679652530913' : '874284439337101',
 	xfbml      : false,
 	status     : true
 };
